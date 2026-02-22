00cb36a9a7a3ac9248c18d03caebcaf3eb89e733
IamdebashisD <adebashisdas626@gmail.com>
2026-02-22 15:23:29 +0530

    Implement custom Array.prototype.myMap with callback support

---
+++ customMapFunction.js
@@ -0,0 +1,15 @@
+Array.prototype.myMap = function (callback) {
+    if (typeof callback !== "function"){
+        throw new TypeError(callback + " is not a function")
+    }
+    const result = new Array(this.length)
+    for (let i = 0; i < this.length; i++) {
+        result[i] = callback(this[i], i , this)
+    }
+    return result
+}
+
+const arr = [1, 2, 3, 4, 5]
+const result = arr.myMap( v => v*10)  
+console.log(result)
+
